f442d4e419a8d522e4a16b868465daa2e953ec19
Jesse Parsons <savvyjesse@aol.com>
2020-08-28 15:40:05 -0400

    add page controller for static pages, add some bootstrap to products#index view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :user
   root "products#index"
   resources :products
+  get '/about', to: "pages#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
